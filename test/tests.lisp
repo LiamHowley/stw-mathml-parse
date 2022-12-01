b0988e4abfa01c30513abcf66bafbca5843aa29d
Liam Howley <liam.howley@hotmail.com>
2022-12-01 23:28:52 +0000

    Adding package to symbols in tests as they're no longer automatically exported.

--- test/tests.lisp
+++ test/tests.lisp
@@ -31,19 +31,19 @@
   :parent test-parse
   (of-type 'mathml-document-node *parsed-mathml*)
   (let ((element (car (get-elements-by-tagname *parsed-mathml* "mo" *mathml-element-class-map*))))
-    (of-type 'mo element)
+    (of-type 'mathml.parse::mo element)
     (of-type 'mathml.parse::mathml-element-class (class-of element)))
   (of-type 'element-node (car (slot-value *parsed-mathml* 'child-nodes)))
-  (of-type 'math (get-element-with-attribute *parsed-mathml* "display"))
+  (of-type 'mathml.parse::math (get-element-with-attribute *parsed-mathml* "display"))
   ;; this is mathml where attribute values can only be a string. This should be false
   (let ((element (get-elements-with-attribute-value *parsed-mathml* "class" "inner-row")))
-    (true (typep (car element) 'mrow))
-    (true (typep (cadr element) 'mrow)))
+    (true (typep (car element) 'mathml.parse::mrow))
+    (true (typep (cadr element) 'mathml.parse::mrow)))
   (let ((element (get-element-with-attribute-values *parsed-mathml* "id" "outer-row")))
-    (true (typep element 'mrow)))
+    (true (typep element 'mathml.parse::mrow)))
   (let ((text-nodes (retrieve-text-nodes *parsed-mathml*)))
     (is string= "∑" (text (car text-nodes)))
-    (of-type 'mrow (get-next-sibling (parent-node (car text-nodes))))))
+    (of-type 'mathml.parse::mrow (get-next-sibling (parent-node (car text-nodes))))))
 
 (define-test reader...
   :parent test-parse
@@ -53,7 +53,7 @@
   (let* ((document-node (read-from-string "<mfrac linethickness=\"0\"><mi>n</mi><mi>k</mi></mfrac>"))
 	 (child-node (car (slot-value document-node 'child-nodes)))
 	 (mi (car (get-elements-by-tagname document-node "mi" *mathml-element-class-map*))))
-    (true (slot-exists-p child-node 'linethickness))
-    (is string= "0" (slot-value child-node 'linethickness))
+    (true (slot-exists-p child-node 'mathml.parse::linethickness))
+    (is string= "0" (slot-value child-node 'mathml.parse::linethickness))
     (is string= "n" (text (car (slot-value mi 'child-nodes))))
     (of-type 'readtable (remove-reader))))
